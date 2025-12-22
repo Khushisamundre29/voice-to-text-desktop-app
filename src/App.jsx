69dc4640f385a56f62fb208b18d00f46fc377687
Khushisamundre29 <khushisamundre04@gmail.com>
2025-12-23 00:26:53 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import { useMicrophone } from "./hooks/useMicrophone";
 import PushToTalk from "./components/PushToTalk";
 import StatusIndicator from "./components/StatusIndicator";
 import TranscriptView from "./components/TranscriptView";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [transcript, setTranscript] = useState("");
+  const clearTranscript = () => {setTranscript("");
+};
+
   const { startRecording, stopRecording, isRecording } = useMicrophone();
 
   return (
-    <div style={{ height: "100vh", width: "100vw", background: "#0f172a", color: "white", padding: 20 }}>
-      <h2 style={{ marginBottom: 50 }}>VoiceToText</h2>
+    <div style={{ height: "100vh", width: "100vw", display: "flex", flexDirection: "column", background: "#0f172a", color: "white" }}>
+      <Navbar />
+      <div style={{ padding: 20 }}></div>
+      <h2 style={{ marginBottom: 5}}></h2>
 
       <div style={{ display: "grid", gridTemplateColumns: "320px 1fr", gap: 20, height: "88%" }}>
         <div
@@ -23,18 +29,27 @@ function App() {
             gap: 20,
             alignItems: "center",
             justifyContent: "center",
-            marginTop: 50,
+            marginTop: 40,
+            marginBottom: 20,
+            marginLeft: 20,
           }}
         >
-          <PushToTalk
+
+            <PushToTalk
             isRecording={isRecording}
-            onStart={() => startRecording()}
-            onStop={() => stopRecording((text) => setTranscript(prev => prev + " " + text))}
-          />
+            onStart={startRecording}
+            onStop={() =>
+            stopRecording((text) =>
+            setTranscript(prev => prev + " " + text)
+            )
+          }
+        />
+
           <StatusIndicator isRecording={isRecording} />
         </div>
 
-        <TranscriptView transcript={transcript} onClear={() => setTranscript("")} />
+        <TranscriptView transcript={transcript} onClear={clearTranscript} />
+
       </div>
     </div>
   );
